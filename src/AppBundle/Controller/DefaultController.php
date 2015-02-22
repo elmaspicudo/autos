6e54046cdbbf5abd8adff0986ceefa3ac8431c7d
elmaspicudo <nypo59@gmail.com>
2015-02-21 22:00:21 -0600

    templates de las paginas

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -8,10 +8,52 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 class DefaultController extends Controller
 {
     /**
-     * @Route("/app/example", name="homepage")
+     * @Route("/", name="homepage")
      */
     public function indexAction()
     {
-        return $this->render('default/index.html.twig');
+         return $this->render('AppBundle:pagina:index.html.twig', array(
+            'entity'      => '',
+        ));
+    }
+
+    /**
+     * @Route("/producto", name="producto")
+     */
+    public function productoAction()
+    {
+         return $this->render('AppBundle:pagina:producto.html.twig', array(
+            'entity'      => '',
+        ));
+    }
+
+    /**
+     * @Route("/interior", name="interior")
+     */
+    public function interiorAction()
+    {
+         return $this->render('AppBundle:pagina:interior.html.twig', array(
+            'entity'      => '',
+        ));
+    }
+
+    /**
+     * @Route("/tips", name="tips")
+     */
+    public function tipsAction()
+    {
+         return $this->render('AppBundle:pagina:tips.html.twig', array(
+            'entity'      => '',
+        ));
+    }
+
+    /**
+     * @Route("/descargas", name="descargas")
+     */
+    public function descargasAction()
+    {
+         return $this->render('AppBundle:pagina:descargas.html.twig', array(
+            'entity'      => '',
+        ));
     }
 }
